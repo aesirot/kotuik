c12daf11788970ba953d8f483412386f250caefa
anton <aesirot@mail.ru>
2021-01-06 20:27:02 +0300

    сокр блокировки

--- src/main/kotlin/bond/CurveBuilder.kt
+++ src/main/kotlin/bond/CurveBuilder.kt
@@ -1,5 +1,6 @@
 package bond
 
+import com.enfernuz.quik.lua.rpc.api.messages.GetQuoteLevel2
 import common.Connector
 import common.StakanProvider
 import model.QuoteType
@@ -22,31 +23,29 @@ class CurveBuilder(private val stakanProvider: StakanProvider) {
     fun build(curve: Curve, settleDate: LocalDate) {
         val prices = HashMap<String, BigDecimal>()
 
-        val rpcClient = Connector.get()
-        synchronized(rpcClient) {
-            for (bond in curve.bonds) {
-                val stakan = stakanProvider.stakan("TQOB", bond.code)
+        for (bond in curve.bonds) {
+            val stakan = stakanProvider.stakan("TQOB", bond.code)
 
-                if (stakan.offers.isEmpty() || stakan.bids.isEmpty()) {
-                    log.error("Cant rebuild curve, stakan ${bond.code} is empty")
-                    return
+            if (stakan.offers.isEmpty() || stakan.bids.isEmpty()) {
+                log.error("Cant rebuild curve, stakan ${bond.code} is empty")
+                return
+            } else {
+                val stakanPrice: BigDecimal
+                //лучший bid последний, лучший offer первый
+                if (curve.quoteType == QuoteType.BID) {
+                    stakanPrice = BigDecimal(stakan.bids[stakan.bids.size - 1].price)
+                } else if (curve.quoteType == QuoteType.MID) {
+                    stakanPrice =
+                        (BigDecimal(stakan.bids[stakan.bids.size - 1].price) + BigDecimal(stakan.offers[0].price))
+                            .divide(BigDecimal(2), 8, RoundingMode.HALF_UP)
                 } else {
-                    val stakanPrice: BigDecimal
-                    //лучший bid последний, лучший offer первый
-                    if (curve.quoteType == QuoteType.BID) {
-                        stakanPrice = BigDecimal(stakan.bids[stakan.bids.size - 1].price)
-                    } else if (curve.quoteType == QuoteType.MID) {
-                        stakanPrice = (BigDecimal(stakan.bids[stakan.bids.size - 1].price) + BigDecimal(stakan.offers[0].price))
-                                .divide(BigDecimal(2), 8, RoundingMode.HALF_UP)
-                    } else {
-                        stakanPrice = BigDecimal(stakan.offers[0].price)
-                    }
-
-                    val nkd = stakanProvider.nkd(bond)
-
-                    val dirtyPrice = stakanPrice + nkd.divide(bond.nominal, 6, RoundingMode.HALF_UP) * BigDecimal(100)
-                    prices[bond.code] = dirtyPrice
+                    stakanPrice = BigDecimal(stakan.offers[0].price)
                 }
+
+                val nkd = stakanProvider.nkd(bond)
+
+                val dirtyPrice = stakanPrice + nkd.divide(bond.nominal, 6, RoundingMode.HALF_UP) * BigDecimal(100)
+                prices[bond.code] = dirtyPrice
             }
         }
 
@@ -63,7 +62,7 @@ class CurveBuilder(private val stakanProvider: StakanProvider) {
         for (bond in curve.bonds) {
             val price = prices[bond.code]
             //?: throw Exception("No price!")
-                    ?: continue
+                ?: continue
 
             val ytm = CalcYield.effectiveYTM(bond, settleDt, price)
 
