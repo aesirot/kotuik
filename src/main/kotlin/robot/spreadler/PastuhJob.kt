2ff1e880e4900c96ece541179c3c8ec618eec829
aesirot <aesirot@mail.ru>
2021-04-25 16:41:20 +0300

    BondQuikChecker

--- src/main/kotlin/robot/spreadler/PastuhJob.kt
+++ src/main/kotlin/robot/spreadler/PastuhJob.kt
@@ -3,10 +3,13 @@ package robot.spreadler
 import bond.YtmOfzDeltaService
 import org.quartz.Job
 import org.quartz.JobExecutionContext
+import robot.orel.BondQuikChecker
 
 class PastuhJob: Job {
     override fun execute(context: JobExecutionContext?) {
         SpreadlerRunner.pastuh()
         YtmOfzDeltaService.initAll()
+        BondQuikChecker.flush()
+        //BondQuikChecker.checkAll()
     }
 }
